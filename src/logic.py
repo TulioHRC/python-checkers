c7971b9f66688bfbed2eef1d3ccf029f2bf6e6d9
TulioHRC <grove1411@gmail.com>
2024-10-26 18:34:46 -0300

    finish

--- src/logic.py
+++ src/logic.py
@@ -1,5 +1,9 @@
-def get_possibilities_to_move_on_place(board, row, column):
+def get_possibilities_to_move_on_place(board, row, column, last_kill):
+  if last_kill != None and (row != last_kill[0] or column != last_kill[1]):
+    return []
   possibilities = []
+  to_kill = 'R' if board[row][column][0] != 'R' else 'B'
+
   if board[row][column][:-1] == 'B':
     if column != 0:
       if board[row-1][column-1] == None:
@@ -7,6 +11,7 @@ def get_possibilities_to_move_on_place(board, row, column):
     if column != 7:
       if board[row-1][column+1] == None:
         possibilities.append((row-1, column+1))
+
   elif board[row][column][:-1] == 'R':
     if column != 0:
       if board[row+1][column-1] == None:
@@ -15,7 +20,125 @@ def get_possibilities_to_move_on_place(board, row, column):
       if board[row+1][column+1] == None:
         possibilities.append((row+1, column+1))
   
-  return possibilities
+  else:
+    r, c = (row, column)
+    while r - 1 >= 0 and c - 1 >= 0: # left north
+      r -= 1
+      c -= 1
+      if board[r][c] == None:
+        possibilities.append((r, c))
+      else: break
+    r, c = (row, column)
+    while r - 1 >= 0 and c + 1 <= 7: # right north
+      r -= 1
+      c += 1
+      if board[r][c] == None:
+        possibilities.append((r, c))
+      else: break
+    r, c = (row, column)
+    while r + 1 <= 7 and c - 1 >= 0: # left south
+      r += 1
+      c -= 1
+      if board[r][c] == None:
+        possibilities.append((r, c))
+      else: break
+    r, c = (row, column)
+    while r + 1 <= 7 and c + 1 <= 7: # right south
+      r += 1
+      c += 1
+      if board[r][c] == None:
+        possibilities.append((r, c))
+      else: break
 
-def can_move(board, _from, _to):
-  print(_from, _to)
+  kills = get_possibilities_to_kill_on_place(board, row, column, to_kill)
+  
+  if len(get_possibilities_to_kill_on_board(board, board[row][column][:-1], to_kill)) > 0 and len(kills) == 0:
+    return []
+
+  return kills if len(kills) > 0 else possibilities
+
+def get_possibilities_to_kill_on_place(board, row, column, to_kill): # Returns a list of possible kills (from, to)
+  kills = []
+
+  if "promoved" in board[row][column]:
+    r, c = (row, column)
+    is_killing = False
+    while r - 1 >= 0 and c - 1 >= 0: # left north
+      r -= 1
+      c -= 1
+      if board[r][c] == None and is_killing:
+        kills.append((r, c))
+      elif board[r][c] and board[r][c][0] == to_kill:
+        if is_killing: break
+        else: is_killing = True
+    r, c = (row, column)
+    is_killing = False
+    while r - 1 >= 0 and c + 1 <= 7: # right north
+      r -= 1
+      c += 1
+      if board[r][c] == None and is_killing:
+        kills.append((r, c))
+      elif board[r][c] and board[r][c][0] == to_kill:
+        if is_killing: break
+        else: is_killing = True
+    r, c = (row, column)
+    is_killing = False
+    while r + 1 <= 7 and c - 1 >= 0: # left south
+      r += 1
+      c -= 1
+      if board[r][c] == None and is_killing:
+        kills.append((r, c))
+      elif board[r][c] and board[r][c][0] == to_kill:
+        if is_killing: break
+        else: is_killing = True
+    r, c = (row, column)
+    is_killing = False
+    while r + 1 <= 7 and c + 1 <= 7: # right south
+      r += 1
+      c += 1
+      if board[r][c] == None and is_killing:
+        kills.append((r, c))
+      elif board[r][c] and board[r][c][0] == to_kill:
+        if is_killing: break
+        else: is_killing = True
+  else:
+    if (row >= 2 and column <= 5) and board[row-1][column+1] and board[row-1][column+1][0] == to_kill and board[row-2][column+2] == None:
+      kills.append((row-2, column+2))
+    if (row >= 2 and column >= 2) and board[row-1][column-1] and board[row-1][column-1][0] == to_kill and board[row-2][column-2] == None:
+      kills.append((row-2, column-2))
+    if (row <= 5 and column <= 5) and board[row+1][column+1] and board[row+1][column+1][0] == to_kill and board[row+2][column+2] == None:
+      kills.append((row+2, column+2))
+    if (row <= 5 and column >= 2) and board[row+1][column-1] and board[row+1][column-1][0] == to_kill and board[row+2][column-2] == None:
+      kills.append((row+2, column-2))
+
+  return kills
+
+
+def get_possibilities_to_kill_on_board(board, turn, to_kill): # Returns a list of possible kills (from, to)
+  kills = []
+  for r in range(8):
+    for c in range(8):
+      if board[r][c] != None and board[r][c][0] == turn:
+        k = get_possibilities_to_kill_on_place(board, r, c, to_kill)
+        for kill in k: kill += (r, c)
+        kills += k
+  
+  return kills
+
+def get_kills_and_turn_none_jumped_cells(board, _from, _to):
+  actual = _from
+  kills = -1
+  while actual[0] != _to[0] and actual[1] != _to[1]:
+    board[actual[0]][actual[1]] = None
+    actual = (actual[0] - 1 if actual[0] > _to[0] else actual[0] + 1, actual[1] - 1 if actual[1] > _to[1] else actual[1] + 1)
+    kills += 1
+
+  return kills
+
+def is_game_over(board, turn):
+  for r in range(8):
+    for c in range(8):
+      if board[r][c] != None and board[r][c][0] == turn:
+        if len(get_possibilities_to_move_on_place(board, r, c, None)) > 0:
+          return False
+  return True
--- src/main.py
+++ src/main.py
@@ -11,6 +11,16 @@ initial_board = [
     [None, 'B', None, 'B', None, 'B', None, 'B'],
     ['B', None, 'B', None, 'B', None, 'B', None]
 ]
+initial_board = [
+    [None, None, None, 'R', None, None, None, 'R'],
+    [None, None, None, None, None, None, 'R', None],
+    [None, 'R', None, None, None, 'B', None, 'R'],
+    [None, None, 'R', None, None, None, None, None],
+    [None, 'B', None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, 'B', None, None, None, None],
+    ['B', None, 'B', None, 'B', None, None, None]
+]
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -51,43 +61,70 @@ def draw_board(board):
 
       if (board[row][column] != None and board[row][column][0] == 'R'):
         pygame.draw.circle(screen, RED, (column * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2, row * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2), SCREEN_CONFIG['square_size'] // 3)
+        if "_promoved_" in board[row][column]:
+          pygame.draw.circle(screen, BLACK, (column * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2, row * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2), SCREEN_CONFIG['square_size'] // 5)
       if (board[row][column] != None and board[row][column][0] == 'B'):
         pygame.draw.circle(screen, BLUE, (column * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2, row * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2), SCREEN_CONFIG['square_size'] // 3)
+        if "_promoved_" in board[row][column]:
+          pygame.draw.circle(screen, BLACK, (column * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2, row * SCREEN_CONFIG['square_size'] + SCREEN_CONFIG['square_size'] // 2), SCREEN_CONFIG['square_size'] // 5)
 
 def draw_status(status):
-  text = my_font.render(F"Turn: {status['turn']}", False, WHITE)
+  pygame.draw.rect(screen, BLACK, (650,20, 500, 500))
+  if status["win"]:
+    text = my_font.render(F"{status['win']} wins!", False, WHITE)
+  else:
+    text = my_font.render(F"Turn: {status['turn']}", False, WHITE)
   screen.blit(text, (650,20))
 
 status = {
-  "turn": "BLUE",
+  "turn": "B",
   "board": initial_board,
-  "selected": None
+  "selected": None,
+  "last_kill": None, # Position of the last killer
+  "win": None,
 }
 
 while True:
   for event in pygame.event.get():
-    if event.type == pygame.MOUSEBUTTONUP:
+    if event.type == pygame.MOUSEBUTTONUP and status["win"] == None:
       pos = pygame.mouse.get_pos()
       
-      for row in range(8):
-        for col in range(8):
-          if status["board"][row][col] != None and status["board"][row][col][-1] == 's':
-            status["board"][row][col] = status["board"][row][col][:-1]
-          elif status["board"][row][col] == 'o': status["board"][row][col] = None
-
       row = pos[1] // 80
       column = pos[0] // 80
+
       if row >= 8 or column >= 8: 
-        selected = None
+        status["selected"] = None
         continue
+      else:
+        if status["board"][row][column] and status["board"][row][column][-1] == 'o':
+          status["board"][row][column] = status["board"][status["selected"][0]][status["selected"][1]]
+          if row == 0 and status["turn"] == 'B':
+            status["board"][row][column] = 'B' + '_promoved_'
+          if row == 7 and status["turn"] == 'R':
+            status["board"][row][column] = 'R' + '_promoved_'
+          kills = logic.get_kills_and_turn_none_jumped_cells(status["board"], status["selected"], (row, column))
+          status["selected"] = None
+          if kills == 0 or len(logic.get_possibilities_to_kill_on_place(status["board"], row, column, 'R' if status["turn"] != 'R' else 'B')) == 0: 
+            status["turn"] = "R" if status["turn"] != 'R' else 'B'
+            status["last_kill"] = None
+          else:
+            status["last_kill"] = (row, column)
+          if logic.is_game_over(status["board"], status["turn"]):
+            status["win"] = "R" if status["turn"] != 'R' else 'B'
+
+      for r in range(8):
+        for c in range(8):
+          if status["board"][r][c] != None and status["board"][r][c][-1] == 's':
+            status["board"][r][c] = status["board"][r][c][:-1]
+          elif status["board"][r][c] == 'o': status["board"][r][c] = None
+
       if (row + column) % 2 != 0 and status["board"][row][column] != None and status["board"][row][column][0] == status["turn"][0] and status["board"][row][column][-1] != "s":
         status["board"][row][column] += "s"
-        selected = (row, column)
-        for possibility in logic.get_possibilities_to_move_on_place(status["board"], row, column):
+        status["selected"] = (row, column)
+        for possibility in logic.get_possibilities_to_move_on_place(status["board"], row, column, status["last_kill"]):
           status["board"][possibility[0]][possibility[1]] = "o"
       else:
-        selected = None
-        
+        status["selected"] = None
 
     if event.type == pygame.QUIT:
       pygame.quit()
